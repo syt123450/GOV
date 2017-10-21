b4aa972702e9ae1600188b0bfb0acaff96a4e05b
syt123450 <421537891@qq.com>
2017-10-20 19:00:45 -0700

    init project structure

---
+++ src/main/java/com/processchecker/ProcessCheckerApplication.java
@@ -0,0 +1,12 @@
+package com.processchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessCheckerApplication.class, args);
+	}
+}
